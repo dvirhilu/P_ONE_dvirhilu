6c90b8468dbb1b0e0ef4c86cd31bae9459c6231d
dvirhilu <dvirhilu16@gmail.com>
2019-06-11 12:39:21 -0600

    copied globals.py to muongun

---
+++ src/exampleSimCode/muongun/globals.py
@@ -0,0 +1,10 @@
+'''
+Define a few useful common parameters
+
+Tom Stuttard
+'''
+
+# Maximum number of files in a dataset
+# This is used to initialise the number of streams to the RNG
+max_num_files_per_dataset = 100000
+
