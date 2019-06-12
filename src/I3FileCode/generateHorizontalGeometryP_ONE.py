ef4f9bb133cb99508134a26441ff8a2874e597af
dvirhilu <dvirhilu16@gmail.com>
2019-06-12 16:29:53 -0600

    finished function generatingLayer

--- src/I3FileCode/generateHorizontalGeometryP_ONE.py
+++ src/I3FileCode/generateHorizontalGeometryP_ONE.py
@@ -4,16 +4,19 @@
 Creates a geometry based on the flat, circular, horizontal
 string P-ONE design. 
 Parameters that can be modified:
-1. number of layers of DOMs
-2. total angle spanned by detector
-3. DOM spacing on each string
-4. Number of DOMS per string
+1. number of layers of DOMs (default 1)
+2. total angle spanned by detector (Default 90 degrees)
+3. DOM spacing on each string (default 100m)
+4. Number of DOMS per string (default 10)
+5. Total DOMs (default 200)
+6. spacing between layers (default 50m)
 '''
 
 from icecube import dataclasses, dataio, icetray
 from icecube.icetray import I3Units, OMKey
 import argparse
 import gcdHelpers
+import numpy as np
 
 parser = argparse.ArgumentParser(description = "Generate a geometry based on the flat, circular, horiztal P-ONE design")
 parser.add_argument('-d', '--domsPerString', dest = 'domsPerString',
@@ -29,22 +32,25 @@ parser.add_argument('-l', '--layers', dest = 'layers',
 parser.add_argument('-o', '--outname', dest = 'outname', 
                     help = "starting z position for the geometry" )
 parser.add_argument('-t', '--totalDoms', dest = 'totalDoms', 
-                    default = 200, help = "total number of doms in the detector" )        
+                    default = 200, help = "total number of doms in the detector" )
+parser.add_argument('-h', '--heightSpacing', dest = 'heightSpacing', 
+                    default = 50, help = "spacing between layers of DOMs" )         
 args = parser.parse_args()
 
 # get parsed arguments
 domsPerString = int(args.domsPerString)
 spacing = float(args.spacing) * I3Units.meter
 zpos = gcdHelpers.convertDepthToZ(float(args.depth)) * I3Units.meter
-xpos = 0
-ypos = 0
+startPos = dataclasses.I3Position(0,0,zpos)
 phi = float(args.angle) * I3Units.deg
 layers = int(args.layers)
 totalDoms = int(args.totalDoms)
+heightSpacing = float(args.heightSpacing)
 
 # calculate parameters from inputs
 domsPerLayer = totalDoms/layers
-stringPerLayer =  
+stringsPerLayer = domsPerLayer/domsPerString 
+dphi = phi/(stringsPerLayer-1)
 
 if args.outname is not None:
     outname = args.outname
@@ -53,5 +59,19 @@ else:
 
 outfile = dataio.I3File('/home/dvirhilu/workFolder/P_ONE_dvirhilu/I3Files/generated/gcd/' + outname, 'w')
 
-def generateLayer():
+def generateLayer(layerNum):
+    stringNumber = layerNum*stringsPerLayer
+    x = startPos.x
+    y = startPos.y
+    z = startpos.z + heightSpacing*layerNum
+    layerMap = dataclasses.I3OMGeoMap()
+    for i in xrange(0, stringsPerLayer):
+        direction = dataclasses.I3Direction(np.cos(i*dphi), np.sin(i*dphi), 0)
+        stringStart = dataclasses.I3Position(x + 50*direction.x, y + 50*direction.y, z)
+        stringMap = gcdHelpers.generateOMString(stringNumber + i, stringStart, domsPerString, spacing, direction)
+        layerMap.update(stringMap)
+    return layerMap
 
+
+
+    
