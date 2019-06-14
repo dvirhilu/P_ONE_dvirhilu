e3761894d1d45af21750e36e7f506d4782cf901e
dvirhilu <dvirhilu16@gmail.com>
2019-06-14 16:52:25 -0600

    all the bugs

--- src/exampleSimCode/muongun/MG_step3_job.sh
+++ src/exampleSimCode/muongun/MG_step3_job.sh
@@ -62,7 +62,7 @@ INFILENAME=MuonGun_step2_${INRUN}_${FILE_NR}.i3.bz2
 INFOLDER=/project/6008051/dvirhilu/P_ONE_dvirhilu/I3Files/generated/muongun_step2
 echo "INFILEPATH: " ${INFOLDER}/$INFILENAME
 OUTFILENAME=MuonGun_step3_${OUTRUN}_${FILE_NR}.i3.bz2
-OUTFOLDER=/project/6008051/dvirhilu/P_ONE_dvirhilu/I3Files/generated/MuonGun_step3
+OUTFOLDER=/project/6008051/dvirhilu/P_ONE_dvirhilu/I3Files/generated/muongun_step3
 echo "OUTFILENAME : " $OUTFILENAME
 GCD_FILE=/project/6008051/hignight/GCD_with_noise/GeoCalibDetectorStatus_AVG_55697-57531_PASS2_SPE_withScaledNoise.i3.gz
 echo "GCD: " $GCD_FILE
