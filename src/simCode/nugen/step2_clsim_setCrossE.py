9bbb34f979c2437b42d82de60c6857bdb383b409
dvirhilu <dvirhilu16@gmail.com>
2019-07-09 17:18:50 -0600

    updated clsim code

--- src/simCode/nugen/step2_clsim_setCrossE.py
+++ src/simCode/nugen/step2_clsim_setCrossE.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
+import argparse
 from os.path import expandvars
 import os, sys, random
 
@@ -9,38 +9,31 @@ import os, sys, random
 # NOTE: There is no bad_dom_cleaning!!!
 #       This you still have to do after the propagation!!!
 
-usage = "usage: %prog [options]"
-parser = OptionParser(usage)
-parser.add_option("-o", "--outfile",default="./test_output.i3",
+parser = argparse.ArgumentParser(description = "Makes I3Photons from step1 of the simulations and propagates them to DOMs")
+parser.add_argument("-o", "--outfile",default="./test_output.i3",
                   dest="OUTFILE", help="Write output to OUTFILE (.i3{.gz} format)")
-parser.add_option("-i", "--infile",default="./test_input.i3",
+parser.add_argument("-i", "--infile",default="./test_input.i3",
                   dest="INFILE", help="Read input from INFILE (.i3{.gz} format)")
-parser.add_option("-r", "--runnumber", type="string", default="1",
+parser.add_argument("-r", "--runnumber", type="string", default="1",
                   dest="RUNNUMBER", help="The run/dataset number for this simulation, is used as seed for random generator")
-parser.add_option("-l", "--filenr",type="string",default="1",
+parser.add_argument("-l", "--filenr",type="string",default="1",
                    dest="FILENR", help="File number, stream of I3SPRNGRandomService")
-parser.add_option("-g", "--gcdfile", default=os.getenv('GCDfile'),
+parser.add_argument("-g", "--gcdfile", default=os.getenv('GCDfile'),
 		  dest="GCDFILE", help="Read in GCD file")
-parser.add_option("-e","--efficiency", type="float",default=1.2, # Using efficiency > 1 as default so we can support systematics sets
+parser.add_argument("-e","--efficiency", type="float",default=1.2, # Using efficiency > 1 as default so we can support systematics sets
                   dest="EFFICIENCY",help="DOM Efficiency ... the same as UnshadowedFraction")
-parser.add_option("-m","--icemodel", default="spice_3.2.1",
+parser.add_argument("-m","--icemodel", default="spice_3.2.1",
                  dest="ICEMODEL",help="Ice model (spice_mie, spice_lea, etc)")
-parser.add_option("-c","--crossenergy", type="float",default=30.0,
+parser.add_argument("-c","--crossenergy", type="float",default=30.0,
                   dest="CROSSENERGY",help="The cross energy where the hybrid clsim approach will be used")
-parser.add_option("-t", action="store_true",  dest="GPU", default=False ,help="Run on GPUs or CPUs")
+parser.add_argument("-t", action="store_true",  dest="GPU", default=False ,help="Run on GPUs or CPUs")
 
 
-(options,args) = parser.parse_args()
-if len(args) != 0:
-        crap = "Got undefined options:"
-        for a in args:
-                crap += a
-                crap += " "
-        parser.error(crap)
+args = parser.parse_args()
 
-options.FILENR=int(options.FILENR)
-options.RUNNUMBER=int(options.RUNNUMBER)
-if options.GPU:
+args.FILENR=int(args.FILENR)
+args.RUNNUMBER=int(args.RUNNUMBER)
+if args.GPU:
         CPU=False
 else:
         CPU=True
@@ -53,20 +46,20 @@ from icecube import phys_services, sim_services
 from icecube import clsim
 
 photon_series = "I3Photons"
-#print 'CUDA devices: ', options.DEVICE
+#print 'CUDA devices: ', args.DEVICE
 tray = I3Tray()
-print 'Using RUNNUMBER: ', options.RUNNUMBER
+print 'Using RUNNUMBER: ', args.RUNNUMBER
 
 # Now fire up the random number generator with that seed
 from globals import max_num_files_per_dataset
 randomService = phys_services.I3SPRNGRandomService(
-    seed = options.RUNNUMBER,
+    seed = args.RUNNUMBER,
     nstreams = max_num_files_per_dataset,
-    streamnum = options.FILENR)
+    streamnum = args.FILENR)
 
 tray.AddService("I3SPRNGRandomServiceFactory","sprngrandom")(
-        ("Seed",options.RUNNUMBER),
-        ("StreamNum",options.FILENR),
+        ("Seed",args.RUNNUMBER),
+        ("StreamNum",args.FILENR),
         ("NStreams", max_num_files_per_dataset),
         ("instatefile",""),
         ("outstatefile",""),
@@ -85,7 +78,7 @@ def BasicHitFilter(frame):
 ### START ###
 
 tray.AddModule('I3Reader', 'reader',
-            FilenameList = [options.GCDFILE, options.INFILE]
+            FilenameList = [args.GCDFILE, args.INFILE]
             )
 
 
@@ -95,20 +88,20 @@ tray.AddModule('I3Reader', 'reader',
 
 tray.AddModule("I3GeometryDecomposer", "I3ModuleGeoMap")
 
-icemodel_path = expandvars("$I3_SRC/ice-models/resources/models/" + options.ICEMODEL)
+icemodel_path = expandvars("$I3_SRC/ice-models/resources/models/" + args.ICEMODEL)
 print 'Medium model ', "ANTARES"
-print "DOM efficiency: ", options.EFFICIENCY
+print "DOM efficiency: ", args.EFFICIENCY
 # Only the photons are made. Still have to convert them to hits!
-print "Setting cross energy: " , float(options.CROSSENERGY), "GeV"
+print "Setting cross energy: " , float(args.CROSSENERGY), "GeV"
 #tray.AddSegment(clsim_hybrid.I3CLSimMakePhotons, 'goCLSIM',
 print "Using CPUs ", CPU
-print "Using GPUs ", options.GPU
+print "Using GPUs ", args.GPU
 
-gcd_file = dataio.I3File(options.GCDFILE)
+gcd_file = dataio.I3File(args.GCDFILE)
 
 tray.AddSegment(clsim.I3CLSimMakePhotons, 'goCLSIM',
                 UseCPUs=CPU,
-                UseGPUs=options.GPU,
+                UseGPUs=args.GPU,
 #		UseOnlyDeviceNumber=[0],
 #                OpenCLDeviceList=[0],
                 MCTreeName="I3MCTree",
@@ -122,13 +115,13 @@ tray.AddSegment(clsim.I3CLSimMakePhotons, 'goCLSIM',
                 #UnWeightedPhotons=True, #turn off optimizations
                 UseGeant4=True,
                 CrossoverEnergyEM=0.1,
-		CrossoverEnergyHadron=float(options.CROSSENERGY),
+		CrossoverEnergyHadron=float(args.CROSSENERGY),
                 StopDetectedPhotons=True,
 #                UseHoleIceParameterization=False, # Apply it when making hits!
 #                HoleIceParameterization=expandvars("$I3_SRC/ice-models/resources/models/angsens/as.flasher_p1_0.30_p2_-1"),
                 DoNotParallelize=False,
                 DOMOversizeFactor=1., 
-                UnshadowedFraction=options.EFFICIENCY, #normal in IC79 and older CLSim versions was 0.9, now it is 1.0
+                UnshadowedFraction=args.EFFICIENCY, #normal in IC79 and older CLSim versions was 0.9, now it is 1.0
                 GCDFile=gcd_file,
                 ExtraArgumentsToI3CLSimModule={
                     #"UseHardcodedDeepCoreSubdetector":True, #may save some GPU memory
@@ -147,7 +140,7 @@ SkipKeys = ["I3MCTree_bak"]
 
 tray.AddModule("I3Writer","writer",
                SkipKeys=SkipKeys,
-               Filename = options.OUTFILE,
+               Filename = args.OUTFILE,
                Streams = [icetray.I3Frame.DAQ, icetray.I3Frame.Physics, icetray.I3Frame.TrayInfo],
               )
 
