72c76b99b25ea448386cf6862d3a77d5f08890c7
dvirhilu <dvirhilu16@gmail.com>
2019-07-11 17:11:00 -0600

    IT WORKED OMG now try with larger file

--- src/simCode/nugen/nugen_step2_job.sh
+++ src/simCode/nugen/nugen_step2_job.sh
@@ -56,12 +56,12 @@ MEDIUMMODEL=/project/6008051/dvirhilu/P_ONE_dvirhilu/propagationMediumModels/${M
 CROSSENERGY=200.0
 echo "MEDIUMMODEL    : " $MEDIUMMODEL
 echo "CROSSENERGY : " $CROSSENERGY
-#INFILENAME=NuGen_step1_${RUNTYPE}_${FILE_NR}.i3.gz
-INFILENAME=testFile.i3.gz
+INFILENAME=NuGen_step1_${RUNTYPE}_${FILE_NR}.i3.gz
+#INFILENAME=testFile.i3.gz
 INFOLDER=/home/dvirhilu/projects/rpp-kenclark/dvirhilu/P_ONE_dvirhilu/I3Files/nugen/nugenStep1
 
 echo "INFILEPATH: " ${INFOLDER}/$INFILENAME
-OUTFILENAME=NuGen_step2_${RUNTYPE}_test1String.i3.gz
+OUTFILENAME=NuGen_step2_${RUNTYPE}_${FILE_NR}.i3.gz
 OUTFOLDER=/home/dvirhilu/projects/rpp-kenclark/dvirhilu/P_ONE_dvirhilu/I3Files/nugen/nugenStep2
 echo "OUTFILEPATH : " ${OUTFOLDER}/$OUTFILENAME
 
