d2450c0a3f3d96500e7835b3f27ee67c16d3baf2
dvirhilu <dvirhilu16@gmail.com>
2019-08-07 15:46:07 -0600

    fixing bugs

--- src/simCode/nugen/nugen_step3_job.sh
+++ src/simCode/nugen/nugen_step3_job.sh
@@ -33,7 +33,7 @@ elif [ "$ISLOCAL" == "false" ]; then
     i3env=/home/users/hignight/oscnext/build_trunk_july_02_2019/env-shell.sh
     echo "Will use i3 environment: " ${i3env}
 
-    export $PYTHONPATH=/home/users/dhilu/P_ONE_dvirhilu/src
+    export $PYTHONPATH=${PYTHONPATH}:/home/users/dhilu/P_ONE_dvirhilu/src
  
 else 
     echo "No configuration for " $ISLOCAL "... exiting"
